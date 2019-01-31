64f5b8404fc8fa829194949dc06bbb3a1fb2b5f5
Caren Keyes <cejkeyes@gmail.com>
2019-01-31 12:15:35 -0800

    made project cards accessbil by adding flip on link focus for the back-side links

--- src/Projects/project-card.js
+++ src/Projects/project-card.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 export default class ProjectCard extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            showBack: false
+        }
+    }
+
+    flipBack = () => {
+        this.setState({showBack: true})
+        console.log('back')
+    }
+
+    flipFront = () => {
+        this.setState({showBack: false})
+        console.log('front')
+    }
+
     render(){
+        let parentDivClass = this.state.showBack? 'project-card-container show-back': 'project-card-container'
         return(
-            <div className='project-card-container'>
+            <div className={parentDivClass}>
                 <div className={`project-card ${this.props.className}`}>
                     <div className='project-card-front'>
                         <img src={this.props.image} alt={this.props.alt}/>
@@ -13,10 +31,17 @@ export default class ProjectCard extends React.Component{
                             <h3>{this.props.title}</h3>
                         </div>
                         <div className='project-links'>
-                            <a href={this.props.github} target="blank" rel="noopener noreferrer" className='github-link image-link'>
+                            <a href={this.props.github} target='blank' rel='noopener noreferrer' 
+                                className='github-link image-link'
+                                onFocus={this.flipBack}
+                                onBlur={this.flipFront}                            
+                            >
                                 Github
                             </a>
-                            <a href={this.props.live} target="blank" rel="noopener noreferrer">VIEW IT LIVE</a>
+                            <a href={this.props.live} target='blank' rel='noopener noreferrer' 
+                                onFocus={this.flipBack}
+                                onBlur={this.flipFront}
+                            >VIEW IT LIVE</a>
                         </div>
                         <div className='project-text'>
                             <p>{this.props.text}</p>
